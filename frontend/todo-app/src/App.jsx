afee5593fa75df578513485eaed27b7cd70d9469
Tsiaro Yohan <tsiaroniainayohan@gmail.com>
2026-02-11 19:18:49 +0300

    Fix daisyUI and Tailwind dans le app react-JS

---
+++ frontend/todo-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Salut Tsiaro</h1>
+      <button className="btn btn-primary">Default</button>
+    </>
+  )
+}
+
+export default App
