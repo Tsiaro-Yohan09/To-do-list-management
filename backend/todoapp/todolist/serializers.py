a5124e05b8ca00370c3837a1e9c2d6f5cfb1dba0
Tsiaro Yohan <tsiaroniainayohan@gmail.com>
2026-02-13 09:37:19 +0300

    I changed the filds to '__all__'

--- backend/todoapp/todolist/serializers.py
+++ backend/todoapp/todolist/serializers.py
@@ -5,6 +5,6 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ["id", "title", "description", "completed", "created"]
+        fields = '__all__'
         
         read_only_fields = ["id", "created"]
